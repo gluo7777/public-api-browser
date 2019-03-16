e1a2b126f2c7b150e27576f36fb2f27beed2e58c
William Luo <gluo7777@gmail.com>
2019-03-15 21:45:40 -0500

    fixed search pane

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,10 +5,12 @@ import Detail from './Detail'
 
 const Search = (props) => (
     <div id="search" className="v-pane">
-        <div className="input-group-prepend">
-            <span className="input-group-text" id="search-text">Search</span>
+        <div className="input-group">
+            <div className="input-group-prepend">
+                <span className="input-group-text" id="search-text">Search</span>
+            </div>
+            <input type="text" className="form-control" placeholder="Type phrases to match title or description" id="search-text"></input>
         </div>
-        <input type="text" className="form-control" placeholder="Type phrases to match title or description" id="search-text"></input>
         <ResultList />
         <Detail />
     </div>
