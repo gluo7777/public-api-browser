d7e6c954dc1f5cbd4e60b7b80415591e527c4e23
William Luo <23488359+gluo7777@users.noreply.github.com>
2019-03-20 17:51:01 -0500

    Modularize Control.js and propagate App.js global state down to children (#5)
    * Modularize Control Components
    
    1. Tabs => Tab
    2. Filter => Separate Filter.js file
    3. Fixed eslint warnings
    
    * (WIP) refactored Filter.js to be modular
    
    1. haven't compiled successfully yet
    2. need to refactor Control.js to pass Global App state down to
    Filter.js
    
    * added search button
    
    * Passed App's state => Control => Filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,36 +1,78 @@
 // Specific Content Components
 import React from 'react'
-const Filter = (props) => (
-    <form>
-        <div className="form-group">
-            <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" id="https" />
-                <label className="form-check-label" htmlFor="https">HTTPS</label>
-            </div>
-            <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" id="cors" />
-                <label className="form-check-label" htmlFor="cors">CORS</label>
-            </div>
-            <small className="form-text text-muted">Filter on endpoints that are HTTPS/CORS enabled.</small>
-        </div>
-        <div className="form-group">
-            <select className="form-control custom-select">
-                <option value="" defaultValue>No Authentication</option>
-                <option value="apikey">API Key</option>
-                <option value="oauth">OAuth</option>
-            </select>
-            <small className="form-text text-muted">What type of authentication is required?</small>
-        </div>
-        <div className="form-group">
-            <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                    <span className="input-group-text" id="category-text">Category</span>
+class Filter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    updateFilterValue(field, value) {
+        const setting = {};
+        setting[field] = value;
+        this.props.onFiltersUpdated(setting);
+        console.info(`Changing ${field} to ${value}.`)
+    }
+    render() {
+        return (
+            <form>
+                <div className="form-group">
+                    <CheckBox label="HTTPS" onValueChanged={value => this.updateFilterValue('https', value)} />
+                    <CheckBox label="CORS" onValueChanged={value => this.updateFilterValue('cors', value)} />
+                    <Description text="Filter on endpoints that are HTTPS/CORS enabled." />
                 </div>
-                <input type="text" className="form-control" placeholder="text" id="category-text" />
-            </div>
-            <small className="form-text text-muted">Include only this category. Not case sensitive.</small>
-        </div>
-    </form>
+                <div className="form-group">
+                    <SelectList
+                        options={[
+                            { label: 'No Authentication', value: '' },
+                            { label: 'API Key', value: 'apikey' },
+                            { label: 'OAuth', value: 'oauth' }
+                        ]}
+                        defaultIndex={0}
+                        onValueChanged={(value => this.updateFilterValue('authentication', value))}
+                    />
+                    <Description text="What type of authentication is required?" />
+                </div>
+                <div className="form-group">
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="category-text">Category</span>
+                        </div>
+                        <input type="text" className="form-control" placeholder="text" id="category-text"
+                            onChange={event => this.updateFilterValue('category', event.target.value)} />
+                    </div>
+                    <Description text="Include only this category. Not case sensitive." />
+                </div>
+            </form>
+        );
+    }
+}
+
+// Specific Form Components
+const CheckBox = (props) => (
+    <div className="form-check form-check-inline">
+        <input className="form-check-input" type="checkbox" id={props.label} onChange={(event) => props.onValueChanged(event.target.checked)} />
+        <label className="form-check-label" htmlFor={props.label}>{props.label}</label>
+    </div>
+);
+
+const SelectList = (props) => (
+    <select className="form-control custom-select" onChange={event => {
+        const list = event.target;
+        const selected = list.selectedOptions[0]; // can only select up to 1 option
+        props.onValueChanged(selected);
+    }}>
+        {buildOptionsList(props.options, props.defaultIndex)}
+    </select>
+);
+
+function buildOptionsList(options, defaultOption = 0) {
+    const lst = options.map((option, index) => <option key={index} value={option.value}>{option.label}</option>);
+    const temp = lst[defaultOption];
+    const option = React.cloneElement(temp);
+    lst[defaultOption] = option;
+    return lst;
+}
+
+const Description = (props) => (
+    <small className="form-text text-muted">{props.text}</small>
 );
 
 export default Filter;
