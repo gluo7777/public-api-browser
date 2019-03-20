8e4e0cf198cafab8e478c23e19fc48ea23641596
William Luo <gluo7777@gmail.com>
2019-03-20 17:13:54 -0500

    added search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,9 +7,10 @@ const Search = (props) => (
     <div id="search" className="v-pane">
         <div className="input-group">
             <div className="input-group-prepend">
-                <span className="input-group-text" id="search-text">Search</span>
+                <span className="input-group-text" id="search-text">Enter Text</span>
             </div>
             <input type="text" className="form-control" placeholder="Type phrases to match title or description" id="search-text"></input>
+            <button type="button" class="btn btn-primary">Search</button>
         </div>
         <ResultList />
         <Detail />
