4e38ba4ee2c2651781f38361f55ec9b0ce0b49c5
William Luo <gluo7777@gmail.com>
2019-03-28 21:40:41 -0500

    Updates

---
+++ precache-manifest.e8e91b949ef1b929c6abbfde5bcb44f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cb7b0ff096d4458804b",
+    "url": "/gluo7777/public-api-browser/static/js/runtime~main.5004db1f.js"
+  },
+  {
+    "revision": "f04b3a29c6061f1dcf54",
+    "url": "/gluo7777/public-api-browser/static/js/main.3b5a4920.chunk.js"
+  },
+  {
+    "revision": "09330c0066b167e46cfd",
+    "url": "/gluo7777/public-api-browser/static/js/2.f0e45a9d.chunk.js"
+  },
+  {
+    "revision": "f04b3a29c6061f1dcf54",
+    "url": "/gluo7777/public-api-browser/static/css/main.56aa889d.chunk.css"
+  },
+  {
+    "revision": "7b512a0c09e4575a6b52447005c049cc",
+    "url": "/gluo7777/public-api-browser/index.html"
+  }
+];
